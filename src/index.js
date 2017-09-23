25888afef8ff786571926f728150d7686b9b0b68
baersc <baerbo@gmail.com>
2017-09-23 17:37:24 -0400

    Fixed link, added Material.io text input field

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 /* eslint-env browser */
 import { MDCRipple } from '@material/ripple';
-import { MDCFormField } from '@material/form-field';
+import { MDCTextfield } from '@material/textfield';
 import './style.scss';
 
 window.onload = () => {
     const buttons = document.getElementsByClassName('mdc-button');
-    for (let i = 0; i < buttons.length; i++) {
+    // const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
+
+    for (let i = 0; i < buttons.length; i += 1) {
         MDCRipple.attachTo(buttons[i]);
     }
-}
+
+    MDCTextfield.attachTo(document.querySelector('.mdc-textfield'));
+};
