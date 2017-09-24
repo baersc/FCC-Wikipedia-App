09b477534c007e62bf62d585301bcc05e78b1c03
baersc <baerbo@gmail.com>
2017-09-23 22:13:06 -0400

    Added further styling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './style.scss';
 
 window.onload = () => {
     const buttons = document.getElementsByClassName('mdc-button');
-    // const formField = new MDCFormField(document.querySelector('.mdc-form-field'));
 
     for (let i = 0; i < buttons.length; i += 1) {
         MDCRipple.attachTo(buttons[i]);
