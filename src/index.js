01efcc4a082ab10b6fe5bd47d4147ed4da36c3de
baersc <baerbo@gmail.com>
2017-12-18 18:53:55 -0500

    commit for computer migration

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import { MDCRipple } from '@material/ripple';
 import { MDCTextfield } from '@material/textfield';
 import './style.scss';
 
-let searchResult;
 
+// Make boxes for search results
+function makeSearch(result) {
+    const page = document.getElementsByClassName('page')[0];
+    const resultsDisp = document.createElement('div');
+    const titleBar = document.createElement('div');
+
+    resultsDisp.classList.add('search-container');
+    titleBar.classList.add('search-title-bar', 'search-results');
+    titleBar.textContent = 'Searched for: '.concat(result[0]);
+
+    page.appendChild(resultsDisp);
+    resultsDisp.appendChild(titleBar);
+
+    for (let i = 0; i < result[1].length; i += 1) {
+        const node = document.createElement('div');
+        const title = '"'.concat(result[1][i], '"');
+        const description = result[2][i];
+        node.classList.add('search-item', 'search-results');
+        node.innerHTML = title + description;
+        titleBar.appendChild(node);
+    }
+}
+
+// AJAX call to Wikipedia, returns 10 results
 function search() {
     const query = document.getElementById('search-field').value;
     const url =
@@ -16,23 +39,20 @@ function search() {
                 'search=',
                 query);
 
-    window.console.log(query, url);
-
     if (window.XMLHttpRequest) {
         const searchRequest = new XMLHttpRequest();
 
         searchRequest.open('GET', url, true);
         searchRequest.send();
         searchRequest.onload = () => {
-            searchResult = searchRequest.responseText;
-            window.console.log(searchRequest);
+            makeSearch(JSON.parse(searchRequest.responseText));
+            window.console.log(JSON.parse(searchRequest.responseText));
         };
     }
 }
 
 window.onload = () => {
     const buttons = document.getElementsByClassName('mdc-button');
-    const searchButton = document.getElementsByClassName('submit-button')[0];
 
     for (let i = 0; i < buttons.length; i += 1) {
         MDCRipple.attachTo(buttons[i]);
@@ -40,18 +60,31 @@ window.onload = () => {
 
     MDCTextfield.attachTo(document.querySelector('.mdc-textfield'));
 
-    searchButton.addEventListener('click', () => search());
+    // Submit-Button
+    document.getElementsByClassName('submit-button')[0]
+        .addEventListener('click',
+            () =>
+                search(),
+        );
 
+    // Clear-Button
     document.getElementsByClassName('clear-button')[0]
         .addEventListener('click',
             () => {
+                const page = document.getElementsByClassName('page')[0];
+                const results = document.getElementsByClassName(
+                    'search-container')[0];
                 const input = document.getElementById('search-field');
+
                 if (input.value !== '') {
                     input.value = '';
                     input.focus();
                 }
+
+                page.removeChild(results);
             });
 
+    // Random-Button
     document.getElementsByClassName('random-button')[0]
         .addEventListener('click',
             () =>
